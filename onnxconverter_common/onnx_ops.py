381a598ae73a638751e8a6930c802aa82f28b98c
xavier dupré <xavier.dupre@gmail.com>
2019-09-04 15:24:14 +0200

    handle min or max = None

--- onnxconverter_common/onnx_ops.py
+++ onnxconverter_common/onnx_ops.py
@@ -192,41 +192,45 @@ def apply_clip(scope, input_name, output_name, container, operator_name=None, ma
             raise RuntimeError("Operator 'Clip': min must be specified if max is.")
         inputs = [input_name]
 
-        if isinstance(min, (np.ndarray, float, int)):
-            # add initializer
-            if isinstance(min, np.ndarray):
-                if min.shape != (1, ):
-                    raise RuntimeError("min must an array of one element.")
-            else:
-                # container in sklearn-onnx stores the computation type in
-                # container.dtype.
-                min = np.array([min], dtype=getattr(
-                    container, 'dtype', np.float32))
-            min_name = scope.get_unique_variable_name('clip_min')
-            container.add_initializer(min_name, getattr(container, 'proto_dtype',
-                onnx_proto.TensorProto.FLOAT), [1], [min[0]])
-            min = min_name
-        if isinstance(min, str):
-            inputs.append(min)
-        else:
-            raise RuntimeError("Parameter 'min' must be a string or a float.")
+        if min is not None:
+            if isinstance(min, (np.ndarray, float, int)):
+                # add initializer
+                if isinstance(min, np.ndarray):
+                    if min.shape != (1, ):
+                        raise RuntimeError("min must an array of one element.")
+                else:
+                    # container in sklearn-onnx stores the computation type in
+                    # container.dtype.
+                    min = np.array([min], dtype=getattr(
+                        container, 'dtype', np.float32))
+                min_name = scope.get_unique_variable_name('clip_min')
+                container.add_initializer(min_name, getattr(container, 'proto_dtype',
+                    onnx_proto.TensorProto.FLOAT), [1], [min[0]])
+                min = min_name
+            if isinstance(min, str):
+                inputs.append(min)
+            elif min is not None:
+                raise RuntimeError("Parameter 'min' must be a string or a float.")
 
-        if isinstance(max, (np.ndarray, float, int)):
-            # add initializer
-            if isinstance(max, np.ndarray):
-                if max.shape != (1, ):
-                    raise RuntimeError("max must an array of one element.")
+        if max is not None:
+            if min is None:
+                raise RuntimeError("Parameter 'min' must be specified if 'max' is.")
+            if isinstance(max, (np.ndarray, float, int)):
+                # add initializer
+                if isinstance(max, np.ndarray):
+                    if max.shape != (1, ):
+                        raise RuntimeError("max must an array of one element.")
+                else:
+                    max = np.array([max], dtype=getattr(
+                        container, 'dtype', np.float32))
+                max_name = scope.get_unique_variable_name('clip_max')
+                container.add_initializer(max_name, getattr(container, 'proto_dtype',
+                    onnx_proto.TensorProto.FLOAT), [1], [max[0]])
+                max = max_name
+            if isinstance(max, str):
+                inputs.append(max)
             else:
-                max = np.array([max], dtype=getattr(
-                    container, 'dtype', np.float32))
-            max_name = scope.get_unique_variable_name('clip_max')
-            container.add_initializer(max_name, getattr(container, 'proto_dtype',
-                onnx_proto.TensorProto.FLOAT), [1], [max[0]])
-            max = max_name
-        if isinstance(max, str):
-            inputs.append(max)
-        else:
-            raise RuntimeError("Parameter 'max' must be a string or a float.")
+                raise RuntimeError("Parameter 'max' must be a string or a float.")
 
         container.add_node('Clip', input_name, output_name, op_version=op_version,
                            **attrs)
