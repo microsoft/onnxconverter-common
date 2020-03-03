70211491e158a0e3f2b3811ffb429c334544a864
David Fan <30608893+jiafatom@users.noreply.github.com>
2020-03-03 10:55:33 -0800

    Handle reserved_names_in_graph in graph optimization (#68)
    * Add reserved_names_in_graph in graph optimization
    
    * Add reserved_names_in_graph in graph optimization

--- onnxconverter_common/_opt_const_folding.py
+++ onnxconverter_common/_opt_const_folding.py
@@ -103,16 +103,17 @@ def _fix_unamed_node(graph):
     return graph
 
 
-def _reserve_node_for_embedded_graph(graph):
+def reserve_node_for_embedded_graph(graph):
     # type: (onnx.GraphProto)->frozenset
+    fixed_graph = _fix_unamed_node(graph)
     ginputs = []
-    for nd_ in graph.node:
+    for nd_ in fixed_graph.node:
         if nd_.op_type in ['Loop', 'Scan']:
             inner_graph = OnnxGraphContext.get_attribute(nd_, 'body')
             inner_inputs = frozenset([i_.name for i_ in inner_graph.input])
             for sub_nd_ in inner_graph.node:
                 ginputs.extend([i_ for i_ in sub_nd_.input if i_ not in inner_inputs])
-    return frozenset(ginputs)
+    return fixed_graph, frozenset(ginputs)
 
 
 def _dfs_calc(graph, node, reserved_names, node_status):
@@ -161,9 +162,8 @@ def _remove_unused_initializers(nodes, initializers, reversed_names):
 
 def const_folding_optimizer(graph):
     # type: (onnx.GraphProto)->onnx.GraphProto
-    fixed_graph = _fix_unamed_node(graph)
+    fixed_graph, reserved_names = reserve_node_for_embedded_graph(graph)
     opt_graph = OnnxGraphContext(fixed_graph)
-    reserved_names = _reserve_node_for_embedded_graph(fixed_graph)
     node_status = {}
     for ts_ in graph.output:
         _dfs_calc(opt_graph, opt_graph.tensor_to_node[ts_.name], reserved_names, node_status)
