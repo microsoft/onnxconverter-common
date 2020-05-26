e3879bab7fb9e5e8707cdcf681ffacb5a814ffb4
Wenbing Li <wenbingl@outlook.com>
2020-05-26 14:15:33 -0700

    fixed length works

--- temp_onnx_fx_test.py
+++ temp_onnx_fx_test.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
         # "Final N loop carried dependency values then K scan_outputs"
         _, range_out = ox.loop(len, is_true, range_body, inputs=[dummy_state_val],
                                outputs=['ds_o', 'range_out'])  # passing is_true for dummy_state
-        return range_out
+        return ox.squeeze(range_out, axes=[1])
 
     onnx_range.save('range.onnx')
     print(onnx_range(np.array(16, dtype=np.int64)))
@@ -117,51 +117,53 @@ if __name__ == '__main__':
 
     # --- greedy search for fixed length of 3
 
-    @Graph.trace(
-        input_types =[_Ty.I(shape=['SOURCE_LENGTH'])],
-        output_types=[_Ty.I(shape=[3])],
-        outputs="Y")
-    def greedy_search_fixed_length_3(X):
-        ox = X.ox
-        data_0 = X
-        data_0_shape = data_0.shape()
-        data_0_mask = ox.constant_of_shape(data_0_shape, value=1.0)
-        seq_len = data_0_shape[-1]
-        data_0_index_range = onnx_range(seq_len).cast(to=ox.float)
-        #max_len = seq_len * 3
+    if True:
+        @Graph.trace(
+            input_types =[_Ty.I(shape=['SOURCE_LENGTH'])],
+            output_types=[_Ty.I(shape=[3])],
+            outputs="Y")
+        def greedy_search_fixed_length_3(X):
+            ox = X.ox
+            data_0 = X
+            data_0_shape = data_0.shape()
+            data_0_mask = ox.constant_of_shape(data_0_shape, value=1.0)
+            seq_len = data_0_shape[-1]
+            data_0_index_range = onnx_range(seq_len).cast(to=ox.float)
+            data_0_index_range = ox.unsqueeze(data_0_index_range, axes=[1,2])
+            #max_len = seq_len * 3
 
-        encoder_context_0 = encode_source(data_0=data_0, data_0_mask=data_0_mask,
-                                          data_0_posrange=data_0_index_range)
+            encoder_context_0 = encode_source(data_0=data_0, data_0_mask=data_0_mask,
+                                              data_0_posrange=data_0_index_range)
 
-        y_len_0 = ox.constant(value=0.0)
-        logp, *out_decoder_states = decode_first(data_1_posrange=y_len_0,
-                                                 encoder_context_0=encoder_context_0, data_0_mask=data_0_mask)
+            y_len_0 = ox.constant(value=0.0)
+            logp, *out_decoder_states = decode_first(data_1_posrange=y_len_0,
+                                                     encoder_context_0=encoder_context_0, data_0_mask=data_0_mask)
 
-        # # !!!! logp[:, :, :, unk_id] = -1e8  # suppress <unk>, like Marian
-        y_t = logp[0, 0].argmax(axis=-1)
-        #test_y_t = (y_t == 0)
-
-        Y = [y_t]
-        for iteration_count in range(1,3):
-            pos = ox.constant(value=iteration_count) + 1
-            data_1_posrange = pos.cast(to=1).unsqueeze(axes=[0, 1, 2])
-            logp, *out_decoder_states = decode_next(
-                prev_word=y_t, data_1_posrange=data_1_posrange,
-                encoder_context_0=encoder_context_0, data_0_mask=data_0_mask,
-                decoder_state_0=out_decoder_states[0], decoder_state_1=out_decoder_states[1],
-                decoder_state_2=out_decoder_states[2], decoder_state_3=out_decoder_states[3],
-                decoder_state_4=out_decoder_states[4], decoder_state_5=out_decoder_states[5])
+            # # !!!! logp[:, :, :, unk_id] = -1e8  # suppress <unk>, like Marian
             y_t = logp[0, 0].argmax(axis=-1)
-            Y += [y_t]
             #test_y_t = (y_t == 0)
 
-        Y = ox.concat(Y, axis=1)
-        return Y
+            Y = [y_t]
+            for iteration_count in range(1,3):
+                pos = ox.constant(value=iteration_count) + 1
+                data_1_posrange = pos.cast(to=1).unsqueeze(axes=[0, 1, 2])
+                logp, *out_decoder_states = decode_next(
+                    prev_word=y_t, data_1_posrange=data_1_posrange,
+                    encoder_context_0=encoder_context_0, data_0_mask=data_0_mask,
+                    decoder_state_0=out_decoder_states[0], decoder_state_1=out_decoder_states[1],
+                    decoder_state_2=out_decoder_states[2], decoder_state_3=out_decoder_states[3],
+                    decoder_state_4=out_decoder_states[4], decoder_state_5=out_decoder_states[5])
+                y_t = logp[0, 0].argmax(axis=-1)
+                Y += [y_t]
+                #test_y_t = (y_t == 0)
 
-    greedy_search_fixed_length_3.save("greedy3.onnx")
+            Y = ox.concat(Y, axis=-1)
+            return Y
 
-    Y = greedy_search_fixed_length_3(np.array([530, 4, 0], dtype=np.int64))[0]
-    print(Y.shape, Y)
+        greedy_search_fixed_length_3.save("greedy3.onnx")
+
+        Y = greedy_search_fixed_length_3(np.array([530, 4, 0], dtype=np.int64))[0]
+        print(Y.shape, Y)
 
     # --- full greedy search
 
@@ -176,6 +178,7 @@ if __name__ == '__main__':
         data_0_mask = ox.constant_of_shape(data_0_shape, value=1.0)
         seq_len = data_0_shape[-1]
         data_0_index_range = onnx_range(seq_len).cast(to=ox.float)
+        data_0_index_range = ox.unsqueeze(data_0_index_range, axes=[1,2])
         max_len = seq_len * 3
 
         encoder_context_0 = encode_source(data_0=data_0, data_0_mask=data_0_mask,
