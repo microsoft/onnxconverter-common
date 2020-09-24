c71ed046fff8c373d4b1c6ee84028aae48df652a
xavier dupré <xavier.dupre@gmail.com>
2020-09-24 23:21:19 +0200

    Update test_data_types.py

--- tests/test_data_types.py
+++ tests/test_data_types.py
@@ -2,7 +2,7 @@ import os
 import onnx
 import unittest
 from distutils.version import StrictVersion
-from onnx.onnx_ml_pb2 import TypeProto
+from onnx import onnx_pb as onnx_proto
 from onnxconverter_common.data_types import (
     BooleanTensorType,
     Complex64TensorType,
@@ -28,7 +28,7 @@ class TestDataTypes(unittest.TestCase):
         dt = data_type((1, 5))
         self.assertIn(data_type.__name__, str(dt))
         assert dt._get_element_onnx_type() > 0
-        assert isinstance(dt.to_onnx_type(), TypeProto)
+        assert isinstance(dt.to_onnx_type(), onnx_proto.TypeProto)
 
     def test_tensor_type(self):
         tensor_types = [
@@ -54,7 +54,7 @@ class TestDataTypes(unittest.TestCase):
     def common_test_data_type(self, data_type):
         dt = data_type()
         self.assertIn(data_type.__name__, str(dt))
-        assert isinstance(dt.to_onnx_type(), TypeProto)
+        assert isinstance(dt.to_onnx_type(), onnx_proto.TypeProto)
 
     def test_data_type(self):
         data_types = [
@@ -68,7 +68,7 @@ class TestDataTypes(unittest.TestCase):
 
     def common_test_seq_type(self, data_type, dt):
         self.assertIn(data_type.__name__, str(dt))
-        assert isinstance(dt.to_onnx_type(), TypeProto)
+        assert isinstance(dt.to_onnx_type(), onnx_proto.TypeProto)
 
     def test_sequence_type(self):
         self.common_test_seq_type(
